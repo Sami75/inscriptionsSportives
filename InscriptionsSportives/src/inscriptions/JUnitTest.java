84f9250f64809cecf8c9c06e1306cae2046cb8d2
nlarbiabdessemeud <nlarbiabdessemeud@A35-08.MDNPEDAGO.local>
2017-11-13 16:53:20 +0100

    Début des tests unitaire avec JUnit

---
+++ InscriptionsSportives/src/inscriptions/JUnitTest.java
@@ -0,0 +1,79 @@
+package inscriptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnitTest {
+
+	@Test
+	void testCompetition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInscriptionsOuvertes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDateCloture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstEnEquipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDateCloture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCandidats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddPersonne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddEquipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
